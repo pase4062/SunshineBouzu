7fbecaca4171125ae5253a65026c04da8abdcac4
kenterrrrrrr <55968857+kenterrrrrrr@users.noreply.github.com>
2020-04-21 21:23:48 +0900

    NextStageに遷移する簡易ゴール作成、かたつむりは途中

---
+++ Assets/Script/Goal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider hit)
+    {
+        if (hit.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("nextStage");
+        }
+    }
+}
