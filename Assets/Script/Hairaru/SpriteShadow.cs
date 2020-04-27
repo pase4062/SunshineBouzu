95d9b7d0d12783316028ad939dc6eff6920c2405
hairaruhirahara <hairaruhirahara@gmail.com>
2020-04-27 22:44:58 +0900

    Assets/Scriptに平原の作業用フォルダを追加と、Spriteの影をつける用のスクリプトを作成しました。SpriteShadow.csは平原以外変更しないでください。また、SpriteShadow.csはまだ未完成のため、使用もしないでください。

---
+++ Assets/Script/Hairaru/SpriteShadow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class SpriteShadow : MonoBehaviour {
+    void Awake() {
+        
+        renderer_.shadowCastingMode = ShadowCastingMode.On;
+        renderer_.receiveShadows    = true;
+    }
+
+    [SerializeField]
+    private SpriteRenderer renderer_ = null;
+}
