27c34cac53eb55e4f96ff771ec3617b4f9ed5ead
hairaruhirahara <hairaruhirahara@gmail.com>
2020-05-08 20:49:38 +0900

    影を落とすための入力用スクリプトを作成しました。基本的にShadowBlockGroupにアタッチして下さい。使用についてはShadowBlockControllerを参照してください。

---
+++ Assets/Script/Hairaru/ShadowBlock/ShadowBlockMover.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Core
+{
+    public class ShadowBlockMover : MonoBehaviour
+    {
+        public void Initialize()
+        {
+            transform_ = transform;
+        }
+
+        public void MoveLeft()
+        {
+            if (transform_.position.x > min_)
+            {
+                transform_.Translate(-speed_, 0.0f, 0.0f);
+
+                var position = transform_.position;
+                if (position.x < min_)
+                {
+                    position.x = min_;
+                    transform_.position = position;
+                }
+            }
+        }
+
+        public void MoveRight()
+        {
+            if (transform_.position.x < max_)
+            {
+                transform_.Translate(speed_, 0.0f, 0.0f);
+
+                var position = transform_.position;
+                if (position.x > max_)
+                {
+                    position.x = max_;
+                    transform_.position = position;
+                }
+            }
+        }
+
+        [SerializeField]
+        private float speed_ = 0.1f;
+
+        [SerializeField]
+        private float max_ = 10.0f;
+
+        [SerializeField]
+        private float min_ = -10.0f;
+
+        private Transform transform_ = null;
+    }
+}
