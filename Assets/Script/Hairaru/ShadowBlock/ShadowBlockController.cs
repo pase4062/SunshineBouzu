ce8b9665a959d9937456df3428ab93c686d9690a
hairaruhirahara <hairaruhirahara@gmail.com>
2020-05-08 20:48:02 +0900

    影を落とすためのブロックの操作スクリプトを作成しました。基本的にはShadowBlockGroup以外で使用は控えてください。またほかのスクリプトを一緒にして使用してください。

---
+++ Assets/Script/Hairaru/ShadowBlock/ShadowBlockController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Core
+{
+    public class ShadowBlockController : MonoBehaviour
+    {
+        private void Awake()
+        {
+            mover_.Initialize();
+        }
+
+        private void Update()
+        {
+            if (input_.GetLeftKey())
+            {
+                mover_.MoveLeft();
+            }
+
+            if (input_.GetRightKey())
+            {
+                mover_.MoveRight();
+            }
+
+            if (input_.GetOpenKey())
+            {
+                door_.Open();
+            }
+
+            if (input_.GetCloseKey())
+            {
+                door_.Close();
+            }
+        }
+
+        [SerializeField]
+        private ShadowBlockMover mover_ = null;
+
+        [SerializeField]
+        private ShadowBlockInput input_ = null;
+
+        [SerializeField]
+        private ShadowBlockDoor door_ = null;
+    }
+}
