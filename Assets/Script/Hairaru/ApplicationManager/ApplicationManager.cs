1b76b693dca1f0527a1bbfbed53e2ddc6c9ca272
hairaruhirahara <hairaruhirahara@gmail.com>
2020-05-18 21:06:10 +0900

    ApplicationManagerスクリプトを作成しました。シーン間で残したいゲームオブジェクトをSceneContentBaseを継承したコンテンツクラスが引き継ぐことができます。ですが、現在まだSceneContentBaseを完全に作っていないので使い方の用途がわかる方だけ使用してください。

---
+++ Assets/Script/Hairaru/ApplicationManager/ApplicationManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Core
+{
+    public class ApplicationManager : MonoBehaviour
+    {
+        public void SetSceneContent(SceneContentBase content)
+        {
+            content_ = content;
+        }
+
+        public T PopSceneContent<T>() where T : SceneContentBase
+        {
+            T result = content_ as T;
+            return result;
+        }
+
+        [RuntimeInitializeOnLoadMethod]
+        private static void Initialize()
+        {
+            var go = new GameObject("ApplicationManager");
+            go.AddComponent<ApplicationManager>();
+        }
+
+        private void Awake()
+        {
+            Debug.Assert(instance_ == null);
+            instance_ = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public static ApplicationManager instance_ = null;
+
+        private SceneContentBase content_ = null;
+    }
+}
